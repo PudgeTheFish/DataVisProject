68efda3c7850645cb9a06fde52967bd504935996
Jonathan Sloan <joanthan@gojourneylife.com>
2019-11-10 20:21:59 -0600

    created dropdown

--- functions.js
+++ functions.js
@@ -10,11 +10,25 @@ function setup_plots() {
 	d3.select('svg').append('g').attr('transform', 'translate(' + pad + ',' + pad + ')').attr('id', 'lines')
 	// group that will contain heatmap (id: hm)
 	d3.select('svg').append('g').attr('transform', 'translate(' + pad + ',' + (20 + pad + height) + ')').attr('id', 'hm')
-	
+
+}
+
+function populate_dropdown() {
+	d3.select('body').append('div').text('Select a question!')	
+	d3.select('body').append('div').append('select').selectAll('questions').data(questions).enter().append('option')
+		.text(d => { console.log(d); return d.question }).attr('value', (d, i) => i);
+	// d3.select('select')
+	// 	.on('change', function (d, i) {
+	// 		var datum = d3.select(this).property('value');
+	// 		sentence = lstm_data[datum].sentence;
+	// 		lstm_states = lstm_data[datum].lstm;
+	// 		setup_vis(); // this is what you will implement - called each time a new sentence is chosen
+	// 	})
 }
 
 function plot_it() {
 
+	populate_dropdown();
 	setup_plots();
 
 }
