a2918137d90cbb78974adca69efb514c27226bd0
PudgeTheFish <samihahislam@gmail.com>
2019-12-04 01:48:40 -0600

    stash changes

--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@
 var width = 1500, height = 1500;
 var pad = 80;
 var width = 1000, height = 1000;
-var pad = 25;
+var pad = 35;
 var lines_width = width / 2 - (2 * pad);
 var lines_height = height / 2 - (2 * pad);
 var data = null;
@@ -17,6 +17,14 @@ var socials = [
 	{ name: "Snapchat", code: "sns2d" },
 	{ name: "YouTube", code: "sns2e" }
 ];
+var socialsMap = {
+	Twitter: "sns2a",
+	Instagram: "sns2b",
+	Facebook: "sns2c",
+	Snapchat: "sns2d",
+	YouTube: "sns2e"
+};
+var barSocials = ["Twitter"];
 var toggleLines = true; // set to true for line graphs, false for bar graphs
 var races = ["White", "Black", "Asian", "Other", "Native American", "Prefer not to answer"]
 
@@ -50,6 +58,7 @@ function init() {
 init();
 
 var handleLinesButt = () => {
+	if (toggleLines) return;
 	toggleLines = true;
 	d3.select("#linesButton").attr("style", "background-color: #35c7f0");
 	d3.select("#barsButton").attr("style", "background-color: #d9d9d9");
@@ -59,6 +68,7 @@ var handleLinesButt = () => {
 };
 
 var handleBarsButt = () => {
+	if (!toggleLines) return;
 	toggleLines = false;
 	d3.select("#linesButton").attr("style", "background-color: #d9d9d9");
 	d3.select("#barsButton").attr("style", "background-color: #35c7f0");
@@ -143,6 +153,36 @@ var setupBarFilters = () => {
 		.property("checked", function (d, i) { return i === 0; });
 };
 
+var handleUpdateSocial = (e) => {
+	if (e.checked) {
+		if (!barSocials.includes(e.id)) {
+			barSocials.push(e.id);
+		}
+	}	
+	if (!e.checked) {
+		barSocials = barSocials.filter(s => s !== e.id); 
+	}
+	console.log(barSocials);
+	plotBars();
+};
+
+var setupBarSocials = () => {
+	d3.select('#filters').append('div').attr('id', 'socialFilters');
+
+	d3.select('#socialFilters').append('form')
+	.selectAll("label")
+	.data(["Twitter", "YouTube"])
+	.enter()
+	.append("label")
+	.text(function (d) { return d; })
+	.insert('input')
+	.attr("type", "checkbox")
+	.attr("id", function (d, i) { return d; })
+	.attr('name', 'mode')
+	.attr("onClick", "handleUpdateSocial(this)")
+	.property("checked", function (d, i) { return i === 0; });
+};
+
 var handleUpdateBars = (e) => {
 	switch (e.id) {
 		case "Sex":
@@ -183,13 +223,42 @@ var handleUpdateBars = (e) => {
 				})
 				.entries(data19);
 			break;
+		plotBars();
 	};
+};
+
+var plotBars = () => {
+	var svg = d3.select('#barsSvg');
+	svg.selectAll('g').remove(); //clear plots
+	let plot = svg.append('g').attr('id', 'plot');
+
+	var socialNum = barSocials.length; // number of social media selected, using all for now
+	var binNum = 5; // number of bins (# of groups of bars)
+
+	var barWidth = 25;
+	var barPad = 7;
+	var outerBarPad = 15;
+	var innerWidth = socialNum * (2*barPad+barWidth);
+	var innerWidthPad = innerWidth + 2*outerBarPad; 
+	var outerWidth = innerWidthPad * binNum;
+
+	var x_inner = d3.scaleBand().domain(barSocials).range([0, innerWidth]).padding(barPad);
+	var x_outer = d3.scaleLinear().domain([0, binNum-1]).range([0, outerWidth]);
+
+	var y_scale = d3.scaleLinear().domain([0, 1]).range([lines_height / 2 - pad, 0]);
+	var colorScale = d3.scaleOrdinal().domain([0, 4]).range(["#E74C3C", "#8E44AD", "#3498DB", "#1ABC9C", "#F39C12"]);
+
+	plot.append('g').attr('id', 'yaxis').call(d3.axisLeft(y_scale));
+	//plot.append('g').attr('id', 'xaxis').call(d3.axisLeft(y_scale));
+
 };
 
 var setup_bar_plots = () => {
 	setupBarFilters();
+	setupBarSocials();
 	d3.select('body').append('svg').attr('width', 1000).attr('height', 1000).attr('transform', 'translate(5,5)').attr("id", "barsParent");
 	d3.select('#barsParent').append('g').attr('transform', 'translate(' + pad + ',' + pad + ')').attr('id', 'barsSvg');
+	plotBars();
 }
 
 //Create SVG elements and perform transforms to prepare for visualization
